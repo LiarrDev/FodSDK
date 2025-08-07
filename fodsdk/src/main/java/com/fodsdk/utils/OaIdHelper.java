997e0d6bca222cdd80169881e14c30da5e3e11d3
Liarr <Liarr@foxmail.com>
2025-08-07 22:18:55 +0800

    Fix ToastUtil

--- fodsdk/src/main/java/com/fodsdk/utils/OaIdHelper.java
+++ fodsdk/src/main/java/com/fodsdk/utils/OaIdHelper.java
@@ -1,7 +1,6 @@
 package com.fodsdk.utils;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.bun.miitmdid.core.ErrorCode;
 import com.bun.miitmdid.core.MdidSdkHelper;
@@ -28,22 +27,24 @@ public class OaIdHelper implements IIdentifierListener {
 
     public void getDeviceIds(Context context) {
         int code = MdidSdkHelper.InitSdk(context, true, this);
-        String TAG = "OaId Error Code";
         switch (code) {
             case ErrorCode.INIT_ERROR_DEVICE_NOSUPPORT: // 不支持的设备
-                Log.e(TAG, "Device Not Support");
+                LogUtil.e("OaId Error - Device Not Support");
                 break;
             case ErrorCode.INIT_ERROR_LOAD_CONFIGFILE:  // 加载配置文件出错
-                Log.e(TAG, "Load Config File ERROR");
+                LogUtil.e("OaId Error - Load Config File ERROR");
                 break;
             case ErrorCode.INIT_ERROR_MANUFACTURER_NOSUPPORT: // 不支持的设备厂商
-                Log.e(TAG, "Manufacturer not Support");
+                LogUtil.e("OaId Error - Manufacturer not Support");
                 break;
             case ErrorCode.INIT_ERROR_RESULT_DELAY: // 获取接口是异步的，结果会在回调中返回，回调执行的回调可能在工作线程
-                Log.e(TAG, "Result Delay");
+                LogUtil.e("OaId Error - Result Delay");
                 break;
             case ErrorCode.INIT_HELPER_CALL_ERROR: // 反射调用出错
-                Log.e(TAG, "Reflect Error");
+                LogUtil.e("OaId Error - Reflect Error");
+                break;
+            default:
+                LogUtil.e("OaId Error - Other Error" + code);
                 break;
         }
     }
--- fodsdk/src/main/java/com/fodsdk/utils/ToastUtil.java
+++ fodsdk/src/main/java/com/fodsdk/utils/ToastUtil.java
@@ -1,15 +1,29 @@
 package com.fodsdk.utils;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.fodsdk.FodBaseApplication;
 
 public class ToastUtil {
+
+    private static final Handler handler = new Handler(Looper.getMainLooper());
+
     public static void show(String msg) {
         if (TextUtils.isEmpty(msg)) {
             return;
         }
-        Toast.makeText(FodBaseApplication.getContext(), msg, Toast.LENGTH_SHORT).show();
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            Toast.makeText(FodBaseApplication.getContext(), msg, Toast.LENGTH_SHORT).show();
+        } else {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(FodBaseApplication.getContext(), msg, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
